c2b2345684e317fba445dc7011510792506e6e08
Kamran Ali <kgali21@gmail.com>
2019-09-12 19:45:20 -0700

    non blocking functions

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,20 @@
 const exec = require('child_process').exec;
 
-function start() {
+function start(response) {
     console.log(`Request handler 'start' was called.`);
-    function sleep(milliSeconds) {
-        const startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + milliSeconds);
-    }
 
-    sleep(10000);
-    return 'Hello Start';
+    exec('ls -lah', function(error, stdout, stderr){
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write(stdout);
+        response.end();
+    });
 }
 
-function upload() {
-    console.log(`Request handler 'upload' was called.`)
-    return 'Hello Upload'
+function upload(response) {
+    console.log(`Request handler 'upload' was called.`);
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello Upload')
+    response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const http = require('http');
 const url = require('url');
 
-function start(route, handle){
-function onRequest(req, response){
-    const pathname = url.parse(req.url).pathname;
-    console.log('Request for ' + pathname + ' received.');
+function start(route, handle) {
+    function onRequest(req, res) {
+        const pathname = url.parse(req.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
 
-    
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    let content = route(handle, pathname)
-    response.write(content);
-    response.end();
-};
 
-http.createServer(onRequest).listen(8888);
-console.log('Server has started.');
+        route(handle, pathname, res);
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
 }
 
 exports.start = start;
