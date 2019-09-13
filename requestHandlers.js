2512863c13338a81149ab82e4624988cf093d169
Kamran Ali <kgali21@gmail.com>
2019-09-12 20:04:38 -0700

    handling post requests

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,6 +3,19 @@ const exec = require('child_process').exec;
 function start(response) {
     console.log(`Request handler 'start' was called.`);
 
+    const body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content=text/html; '+
+    'charset=utf-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
     exec('ls -lah', function(error, stdout, stderr){
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write(stdout);
--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const url = require('url');
 
 function start(route, handle) {
     function onRequest(req, res) {
+        const postData = '';
         const pathname = url.parse(req.url).pathname;
         console.log('Request for ' + pathname + ' received.');
 
+        req.setEncoding(utf8);
 
-        route(handle, pathname, res);
+        req.addListener('data', function(postDataChunk) {
+            postData += postDataChunk;
+            console.log('Received POST data chunk '+
+            postDataChunk + '.');
+        });
+
+        requestAnimationFrame.addListener('end', function(){
+            route(handle, pathname, res, postData);
+        })
+
     }
 
     http.createServer(onRequest).listen(8888);
